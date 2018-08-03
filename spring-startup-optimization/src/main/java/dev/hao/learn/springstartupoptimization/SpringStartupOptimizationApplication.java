716bd80bf3215a5398f985b48b08105b0d37603b
kuangdev <kuanghaochina@gmail.com>
2018-08-02 22:22:01 -0400

    add project

---
+++ spring-startup-optimization/src/main/java/dev/hao/learn/springstartupoptimization/SpringStartupOptimizationApplication.java
@@ -0,0 +1,12 @@
+package dev.hao.learn.springstartupoptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartupOptimizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartupOptimizationApplication.class, args);
+	}
+}
